b2a23b965d2aedb0163fbbbd24282f4026247909
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-01-22 14:09:44 -0700

    saved count variable to local storage

--- assets/logic.js
+++ assets/logic.js
@@ -4,10 +4,14 @@ var quizEl = document.getElementById("quiz");
 var questEl = document.getElementById("quest")
 
 var questions = [ {
-  question:"How you doing?", one:"Good", two:"Bad", three:"meh", four:"help", answer:"Bad"
+  question:"'hello' is an example of which data type?", one:"string", two:"array", three:"object", four:"boolean", answer:"string"
 }, {
-  question:"Will Tomorrow be better?", one:"unlikely", two:"why bother", three:"no chance", four:"give up now", answer:"unlikely"
-},
+  question:"What language is responsible for style associated with a web page?", one:"JavaScrpt", two:"Bootstrap", three:"CSS", four:"HTML", answer:"CSS"
+}, {
+  question:"A true or false value is what data type?", one:"string", two:"array", three:"object", four:"boolean", answer:"boolean"
+}, {
+  question:"Which of the following is not a coding language?", one:"Javascript", two:"Bootstrap", three:"CSS", four:"HTML", answer:"Bootstrap"
+}
 ]
 var ans1 = document.createElement("button");
 var ans2 = document.createElement("button");
@@ -18,7 +22,8 @@ var res5 = document.createElement("div");
 
 var i = 0;
 
-
+var score = localStorage.getItem("score")
+console.log(score);
 var time = 100;
 
 timeEl.textContent = "Time: " + time;
@@ -36,7 +41,6 @@ quizEl.addEventListener("click", function() {
     }
   }, 1000);
 
-  var score = 0;
 
   quizEl.setAttribute("style", "display:none");
   titQuesEl.textContent = questions[i].question;
@@ -57,11 +61,16 @@ quizEl.addEventListener("click", function() {
       score++;
       res5.innerHTML = "correct";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -74,11 +83,16 @@ quizEl.addEventListener("click", function() {
       time = time - 5;
       res5.innerHTML = "wrong";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -96,11 +110,16 @@ quizEl.addEventListener("click", function() {
       score++;
       res5.innerHTML = "correct";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -113,11 +132,16 @@ quizEl.addEventListener("click", function() {
       time = time - 5;
       res5.innerHTML = "wrong";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -131,13 +155,19 @@ quizEl.addEventListener("click", function() {
   ans3.addEventListener("click", function() {
     if (ans3.innerHTML === questions[i].answer) {
       i++;
+      score++;
       res5.innerHTML = "correct";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -150,11 +180,16 @@ quizEl.addEventListener("click", function() {
       time = time - 5;
       res5.innerHTML = "wrong";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -169,13 +204,19 @@ quizEl.addEventListener("click", function() {
   ans4.addEventListener("click", function() {
     if (ans4.innerHTML === questions[i].answer) {
       i++;
+      score++;
       res5.innerHTML = "correct";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
@@ -188,11 +229,16 @@ quizEl.addEventListener("click", function() {
       time = time - 5;
       res5.innerHTML = "wrong";
       if (i === questions.length) {
+        clearInterval(timerInterval);
         titQuesEl.textContent = "Finished!"
         ans1.remove();
         ans2.remove();
         ans3.remove();
         ans4.remove();
+        var bonus = time;
+        score = score + bonus;
+        res5.innerHTML = "Score: " + score;
+        localStorage.setItem("score", score);
       } else if (i < questions.length) {
         titQuesEl.textContent = questions[i].question;
         ans1.innerHTML = questions[i].one;
